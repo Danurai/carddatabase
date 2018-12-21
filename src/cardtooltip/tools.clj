b23fdaecef3a0cbdfd068ad7d491a13cc95d00f0
danurai <danurai74@gmail.com>
2018-12-21 18:16:46 +0000

    remove buf

--- src/cardtooltip/tools.clj
+++ src/cardtooltip/tools.clj
@@ -11,21 +11,16 @@
 
 (def MAX_SUPPORTED_VERSION 1)
 (def ^:const BYTES_PER_CARD 5)
-(def wh-spec (buf/spec :count buf/byte :id buf/int32))
+
+;(def wh-spec (buf/spec :count buf/byte :id buf/int32))
 
 ; RYO instad of octet
 ;
-;(defn toUint32 [ta]
-;  (bit-or (bit-shift-left (aget ta 3) 24)
-;          (bit-shift-left (bit-and 0xff (aget ta 2)) 16)
-;          (bit-shift-left (bit-and 0xff (aget ta 1)) 8)
-;          (bit-and 0xff (aget ta 0))))
-;(defn getCards [data]
-;  (let [cardData (-> data second byte-array)]
-;    (prn cardData)
-;    (for [i (range 0 (count cardData) 5)]
-;      (assoc {} :count (aget cardData i) 
-;                :id (-> cardData (nthrest (inc i)) byte-array toUint32x)))))
+(defn toUint32 [ta]
+  (bit-or (bit-shift-left (bit-and 0xff (aget ta 3)) 24)
+          (bit-shift-left (bit-and 0xff (aget ta 2)) 16)
+          (bit-shift-left (bit-and 0xff (aget ta 1)) 8)
+          (bit-and 0xff (aget ta 0))))
 
 (defn- fromBase64 [b64] 
   (.decode (Base64/getDecoder) b64))
@@ -41,17 +36,24 @@
         [(nth view 1) (nthrest view 2)]
         [0 view])))        
         
-(defn- getCards [cards]
-  (let [n (count cards)
-        buffer (buf/allocate n)]
-    (buf/write! buffer (byte-array cards) (buf/repeat n buf/byte))
-    (for [i (range 0 n BYTES_PER_CARD)]
-      (buf/with-byte-order :little-endian
-        (buf/read buffer wh-spec {:offset i})))))
+;(defn- getCards [cards]
+;  (let [n (count cards)
+;        buffer (buf/allocate n)]
+;    (buf/write! buffer (byte-array cards) (buf/repeat n buf/byte))
+;    (for [i (range 0 n BYTES_PER_CARD)]
+;      (buf/with-byte-order :little-endian
+;        (buf/read buffer wh-spec {:offset i})))))
+(defn getCards [data]
+  (let [cardData (-> data second byte-array)]
+    (prn cardData)
+    (for [i (range 0 (count cardData) 5)]
+      (assoc {} :count (aget cardData i) 
+                :id (-> cardData (nthrest (inc i)) byte-array toUint32)))))
         
 (defn parsedeck [deckcode]
   (let [[version cards] (-> deckcode
                             parseQueryStringDeckCode
                             fromBase64
                             getVersionAndData)]
-    {:version version :cards (getCards cards)}))
+;    {:version version :cards (getCards cards)}))
+    {:version version :cards cards}))
